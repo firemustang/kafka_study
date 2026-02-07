56e3c1f6826233f5024be96b8cacdac5094415de
firemustang <jeongeuuun@gmail.com>
2026-02-08 00:46:03 +0900

    config commit

--- src/main/kotlin/config/KafkaConfig.kt
+++ src/main/kotlin/config/KafkaConfig.kt
@@ -20,9 +20,15 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
 import org.springframework.kafka.listener.ContainerProperties
+import org.springframework.kafka.listener.DefaultErrorHandler
+
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
 import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.kafka.support.serializer.JsonSerializer
+import org.springframework.retry.backoff.ExponentialBackOffPolicy
+import org.springframework.retry.policy.SimpleRetryPolicy
+import org.springframework.retry.support.RetryTemplate
+import kotlin.time.Duration
 
 @Configuration
 class KafkaConfig(
@@ -39,11 +45,18 @@ class KafkaConfig(
     @Bean
     fun orderEventConsumerFactory(): ConsumerFactory<String, OrderEvent> {
         val props = mapOf(
+            // 접속을 하고자 하는 부트스트랩 서버
             ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            /**
+             * 에러 핸들링 key-value
+             * ErrorHandlingDeserializer을 사용하게 된다면 데코레이터 패턴 적용. 이 패턴 사용 시, 역직렬화 실패가 나면
+             * 애플리케이션 중단을 방지 할 수 있음
+             */
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
             ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
             ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS to StringDeserializer::class.java,
             ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS to JsonDeserializer::class.java,
+
             JsonDeserializer.VALUE_DEFAULT_TYPE to OrderEvent::class.java,
             JsonDeserializer.TRUSTED_PACKAGES to "*",
             JsonDeserializer.USE_TYPE_INFO_HEADERS to false
@@ -52,10 +65,37 @@ class KafkaConfig(
         return DefaultKafkaConsumerFactory(props)
     }
 
+    /*
+    * KafkaListener 어노테이션의 메시지 수신에 적용
+    * ConcurrentKafkaListenerContainerFactory를 사용하여 구현
+    * 고급 설정 추가로 구현 가능
+    * - 동시 컨슈머 수
+    * - ack 모드
+    */
     @Bean
     fun orderEventKafkaListenerContainerFactory() : ConcurrentKafkaListenerContainerFactory<String, OrderEvent> {
         val factory = ConcurrentKafkaListenerContainerFactory<String, OrderEvent>()
         factory.consumerFactory = orderEventConsumerFactory()
+
+        /* 고급 설정
+        factory.setConcurrency(1) // 동시 Consumer 수
+        factory.containerProperties.ackMode = ContainerProperties.AckMode.BATCH
+        factory.containerProperties.ackTime = Duration.ofSeconds(1)
+        factory.containerProperties.pollTimeout = Duration.ofSeconds(5)
+
+        // 에러 핸들링 (예시)
+        factory.setCommonErrorHandler(DefaultErrorHandler().apply {
+            setRetryTemplate(RetryTemplate().apply {
+                setRetryPolicy(SimpleRetryPolicy(3))
+                setBackOffPolicy(ExponentialBackOffPolicy().apply {
+                    initialInterval =1000
+                    multiplier = 2.0
+                    maxInterval = 1000
+                })
+            })
+        })
+        */
+
         return factory
     }
 
@@ -88,7 +128,7 @@ class KafkaConfig(
             ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
             ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
-            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to true,
+            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to true, //autocommit true로 설정
             ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG to 1000
         )
         return DefaultKafkaConsumerFactory(props)
@@ -149,11 +189,11 @@ class KafkaConfig(
             ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to KafkaAvroSerializer::class.java,
             AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to SCHEMA_REGISTRY_URL,
             AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS to true,
-            ProducerConfig.ACKS_CONFIG to "1", // "0", "1", "all"
+            ProducerConfig.ACKS_CONFIG to "1", // "0": 브로커한테 메시지 전송됐는지 확인 안함, "1" :리더한테만 , "all" : 모든 follower까지(금융, 결제 시스템등)
             ProducerConfig.RETRIES_CONFIG to 3,
             ProducerConfig.BATCH_SIZE_CONFIG to 16384,
             ProducerConfig.LINGER_MS_CONFIG to 10,
-            ProducerConfig.COMPRESSION_TYPE_CONFIG to "snappy", // none, lz4, gzip, zstd
+            ProducerConfig.COMPRESSION_TYPE_CONFIG to "snappy", // none(0%), lz4(40~60%), gzip, zstd(75~80%)
         )
 
         return DefaultKafkaProducerFactory(props)
